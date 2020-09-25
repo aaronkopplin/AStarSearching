59765803a01d4db3935f14081d4ae538cce208e7
aaron <kopplina@mail.gvsu.edu>
2020-09-25 17:49:39 -0400

    set up grid

--- Cell.py
+++ Cell.py
@@ -1,13 +1,15 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QSizePolicy
 
 
 class Cell(QtWidgets.QPushButton):
     def __init__(self):
         super().__init__()
         self.setStyleSheet("background-color : white;\n"
-                           "border : 5px solid;\n"
-                           "border-top-color : red;\n"
-                           "border-left-color : orange;\n"
-                           "border-bottom-color : yellow;\n"
-                           "border-right-color : green;\n"
+                           "border : .5px solid;\n"
+                           "border-top-color : rgb(200, 200, 200);\n"
+                           "border-left-color : rgb(200, 200, 200);\n"
+                           "border-bottom-color : rgb(200, 200, 200);\n"
+                           "border-right-color : rgb(200, 200, 200);\n"
                            "")
+        self.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
--- InformedSearch.py
+++ InformedSearch.py
@@ -1,32 +1,30 @@
 import sys
 from PyQt5.QtWidgets import QGridLayout, QWidget, QApplication
+from PyQt5.QtCore import QRect
 import Cell
-
-
-# qtCreatorFile = "maze-window.ui"  # Enter file here.
-#
-# Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
 class Maze(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setGeometry(QRect(0, 0, 650, 650))
+        self.init_ui()
         self.show()
 
-
-    def initUI(self):
+    def init_ui(self):
         grid = QGridLayout()
+        grid.setHorizontalSpacing(0)
+        grid.setVerticalSpacing(0)
         self.setLayout(grid)
+        for alpha in range(16):  # X axis
+            for numeric in range(1, 17):  # Y axis
+                c = Cell.Cell()
+                if alpha == 0 or numeric == 1:
+                    c.setText(chr(97 + alpha) + ", " + str(numeric))
+                grid.addWidget(c, numeric, alpha)
 
-
-        grid.addWidget(Cell.Cell(), 0, 0)
-        grid.addWidget(Cell.Cell(), 0, 1)
-        grid.addWidget(Cell.Cell(), 1, 0)
-        grid.addWidget(Cell.Cell(), 1, 1)
-
-
-
+    def keyPressEvent(self, event):
+        print(event.key())
 
 
 if __name__ == "__main__":
